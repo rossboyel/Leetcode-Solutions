2b9ed72b7d1b586243741fb209eb1d51d7a4990b
rossboyel <rossboylegla@gmail.com>
2022-09-19 20:30:53 +0100

    Successful solution - 1ms & 42.7mb

---
+++ ConcatenationOfArray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class ConcatenationOfArray {
+
+    public static int[] getConcatenation(int[] nums) {
+
+        int[] numsArr = new int[nums.length * 2];
+
+        for(int i = 0; i < nums.length; i++) {
+            numsArr[i] = nums[i];
+            numsArr[i + nums.length] = nums[i];
+            }
+
+        return numsArr;
+    }
+
+}
