bcd03002ddd8dab0147dfa168d951fb83f080488
rossboyel <rossboylegla@gmail.com>
2022-09-26 16:33:06 +0100

    Successful solution - 1ms & 41.4mb

---
+++ NumberOfSteps.java
@@ -0,0 +1,19 @@
+public class NumberOfSteps {
+
+    public static int numberOfSteps(int num) {
+        int count = 0;
+
+        while(num != 0) {
+            if(num % 2 != 0) {
+                num = num - 1;
+                count = count+ 1;
+            }
+            else if (num % 2 == 0 ) {
+                num = num /2;
+                count = count + 1;
+            }
+        }
+
+        return count;
+    }
+}
