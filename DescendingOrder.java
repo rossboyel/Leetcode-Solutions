c3948cec1fd02f7d0d28239ba615a72c8da52b78
rossboyel <rossboylegla@gmail.com>
2022-10-08 13:58:40 +0100

    Codewars Solution  - Descending Order

---
+++ DescendingOrder.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        String[] array = String.valueOf(num).split("");
+        Arrays.sort(array, Collections.reverseOrder());
+        return Integer.parseInt(String.join("", array));
+    }
+}
