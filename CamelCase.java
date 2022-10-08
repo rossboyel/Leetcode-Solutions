b4ffb1a5438c94fb06adbcb1730c231cc40b9ae4
rossboyel <rossboylegla@gmail.com>
2022-10-08 18:52:12 +0100

    Codewars Solution  - CamelCase

---
+++ CamelCase.java
@@ -0,0 +1,14 @@
+
+public class CamelCase {
+    public static String camelCase(String input) {
+        StringBuilder ans = new StringBuilder();
+        for(char ch: input.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                ans.append(" ").append(ch);
+            } else {
+                ans.append(ch);
+            }
+        }
+        return ans.toString();
+    }
+}
